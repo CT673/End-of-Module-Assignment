662b8575d3f3281a5cb84d2071a2e2a8a3c21e45
CT673 <108407338+CT673@users.noreply.github.com>
2022-12-18 07:46:46 +1000

    Update Client.py

--- Client.py
+++ Client.py
@@ -8,47 +8,15 @@ from dict2xml import dict2xml
 from cryptography.fernet import Fernet
 COUNTRIES = Config.countries
 
-# Server connection
-def connect_to_server(self):
-    s = socket.socket()
-    client = socket.socket()
-    client.connect((Config.server_address, Config.port_number))
-    name = input("Enter your name, please: ")
-    client.send(bytes(name, 'utf-8'))
-    data = input("Please type dictionary: ")
-    client.send(bytes(data, 'utf-8'))
-    print("Welcome to the dictionary:")
+s = socket.socket()
+client = socket.socket()
+client.connect((Config.server_address, Config.port_number))
+print("Welcome to the dictionary:")
 
-    for key, value in COUNTRIES.items():
-      print(key, value, '\n')
+for key, value in COUNTRIES.items():
+    print(key, value)
 
-# Server disconnect
-def disconnect(self):
-    if self.client_socket is not None:
-        self.client_socket.send(f"EXIT".encode())
-        self.client_socket.close()
-
-# Edit dictionary
-def edit(self, key, value):
-    self.dictionary[key] = value
-
-# Create new dictionary
- def create_new_dictionary(self):
-     self.dictionary = {}
-
-# File encryption
-def encrypt(filename, key):
-    #pass an extra variable containing the info we are sending .dumps
-    f = Fernet(key)
-    with open(filename, "rb") as file: 
-        # read all file data
-        file_data = file.read()
-    # encrypt data
-    encrypted_data = f.encrypt(file_data)
-    # write the encrypted file
-    with open(filename, "wb") as file:
-        file.write(encrypted_data)
-
+    
 # Serialization/ pickle file
 def serialise_dictionary(self, serialization):
     if Config.serialization_option == "JSON":
@@ -66,24 +34,3 @@ def serialise_dictionary(self, serialization):
             f.close()
             print('Dictionary serialised as XML. Filename: xml')
             #we do not need to write to files, we can return either JSON, pickle or XML
-
-def display_dictionary(self):
-    print(self.dictionary)
-
-# Send file to server
-# instead of filename, we pass the information directly to that function
-def send_file(self, filename, encrypted_data):
-    filesize = os.path.getsize(filename)
-    self.client.send(f"{filename} \ {filesize} \ {encrypted_data}".encode())
-    with open(filename, "rb") as file:
-        while True:
-            bytes_read = file.read(Config.Buffer_size)
-            if not bytes_read:
-                break
-            self.client_socket.sendall(bytes_read)
-    print(filename + ' was successfully sent to the server.')
-
-
-
-
-
