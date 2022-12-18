30201fc557dfc8d9c90f786350d6109ceafce0c5
CT673 <108407338+CT673@users.noreply.github.com>
2022-12-18 21:34:09 +1000

    Update Server.py

--- Server.py
+++ Server.py
@@ -10,10 +10,11 @@ s.bind((Config.server_address, Config.port_number))
 s.listen(3)  # take it out or put it into the config file (config.timeout(3))
 print('Waiting for connections')
 
-class Ture:
-    pass
-while Ture:
+while True:
     c, addr = s.accept()
     name = c.recv(1024).decode()  # put the number into the config file # print("connected with ", addr, name)
+    if Config.opt == 1:
+        print(name)
+    else: 
     c.send(bytes('Welcome to the dictionary', 'utf-8'))  # messages into the config file
     c.close()
