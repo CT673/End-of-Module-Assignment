b1d4811bb463e5ceaf0060603a781a2c518d76dc
CT673 <108407338+CT673@users.noreply.github.com>
2022-12-18 23:21:57 +1000

    Update Server.py

--- Server.py
+++ Server.py
@@ -10,12 +10,32 @@ s.bind((Config.server_address, Config.port_number))
 s.listen(3)  # take it out or put it into the config file (config.timeout(3))
 print('Waiting for connections')
 
+# Deserialization
+def des(deserialization):
+    if Config.deserialization_option == "JSON":
+        return json.loads(deserialization)
+
+    elif Config.deserialization_option == "BINARY":
+        return pickle.loads(deserialization)
+
+    elif Config.deserialization_option == "XML":
+        return dict2xml(deserialization, wrap="dictionary", indent="  ")
+
+    else:
+        print("format not recognised")
+
+
+payload = des(COUNTRIES)
+s.recv(payload)
 while True:
-    c, addr = s.accept()
+       c, addr = s.accept()
     name = c.recv(1024).decode()  # put the number into the config file # print("connected with ", addr, name)
     if Config.opt == 1:
         print(name)
-    else: 
+    else:
+        sys.exit()
         
     c.send(bytes('Welcome to the dictionary', 'utf-8'))  # messages into the config file
     c.close()
+
+    
