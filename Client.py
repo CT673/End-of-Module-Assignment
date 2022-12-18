7b0113d3e6a1b1f29cd34b349409db521184b212
CT673 <108407338+CT673@users.noreply.github.com>
2022-12-18 21:20:14 +1000

    Update Client.py

--- Client.py
+++ Client.py
@@ -27,14 +27,17 @@ def serialise_dictionary(self, serialization):
         with open('dico.bin', 'wb') as f:
             pickle.dump(self.dictionary, f)
             print('Dictionary serialised. Filename: Binary')
-    elif format == "XML":
+    elif Config.serialization_option == "XML":
         xml_content = dict2xml(self.dictionary, wrap="dictionary", indent="  ")
         with open('dico.xml', 'w') as f:
             f.write(xml_content)
             f.close()
             print('Dictionary serialised as XML. Filename: xml')
             #we do not need to write to files, we can return either JSON, pickle or XML
-            
+    else:
+        print("format not recognised")
+              
+              
 # Edit dictionary
 class old_dictionary(COUNTRIES):
     def __init__(self):
