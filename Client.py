9ac2c9c03d0bcd22236a9204abfaa155756de0eb
CT673 <108407338+CT673@users.noreply.github.com>
2022-12-18 21:29:55 +1000

    Update Client.py

--- Client.py
+++ Client.py
@@ -8,48 +8,23 @@ from dict2xml import dict2xml
 from cryptography.fernet import Fernet
 COUNTRIES = Config.countries
 
+# Serialization/ pickle file
+def serialise_dictionary(serialization):
+    if Config.serialization_option == "JSON":
+            return json.dumps(serialization)
+            
+    elif Config.serialization_option == "BINARY":
+       return pickle.dumps(serialization)
+          
+    elif Config.serialization_option == "XML":
+        return dict2xml(serialization, wrap="dictionary", indent="  ")
+       
+    else:
+        print("format not recognised")
+        
 s = socket.socket()
 client = socket.socket()
 client.connect((Config.server_address, Config.port_number))
-print("Welcome to the dictionary:")
-
-for key, value in COUNTRIES.items():
-    print(key, value)
-
+payload = serialization_dictionary(COUNTRIES)
+client.sent(payload)
     
-# Serialization/ pickle file
-def serialise_dictionary(self, serialization):
-    if Config.serialization_option == "JSON":
-        with open('dico.json', 'w') as f:
-            json.dump(self.dictionary, f)
-            print('Dictionary serialised. Filename: JSON')
-    elif Config.serialization_option == "BINARY":
-        with open('dico.bin', 'wb') as f:
-            pickle.dump(self.dictionary, f)
-            print('Dictionary serialised. Filename: Binary')
-    elif Config.serialization_option == "XML":
-        xml_content = dict2xml(self.dictionary, wrap="dictionary", indent="  ")
-        with open('dico.xml', 'w') as f:
-            f.write(xml_content)
-            f.close()
-            print('Dictionary serialised as XML. Filename: xml')
-            #we do not need to write to files, we can return either JSON, pickle or XML
-    else:
-        print("format not recognised")
-              
-              
-# Edit dictionary
-class old_dictionary(COUNTRIES):
-    def __init__(self):
-        self = COUNTRIES()
-
-    def add(self, key, value):
-        self[key] = value
-
-
-edits = old_dictionary()
-edits.key = input("")
-edits.values = input("")
-edits.add(edits.key, edits.value)
-
-print(edits)
