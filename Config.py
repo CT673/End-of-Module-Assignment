cce0049e8a3a23fd1809a8aca70f85d2cb12fbc1
JuanCRuizA <j.ruiz-arteaga@liverpool.ac.uk>
2022-12-18 19:51:23 -0500

    Modify the concept joining the server with the client, and printing in different formats: txt, txt encrypted, Json, console and XML.

--- Config.py
+++ Config.py
@@ -1,5 +1,5 @@
 # Creating a dictionary of 20 countries
-countries = dict({'1': 'Argentina',
+Countries = dict({'1': 'Argentina',
                   '2': 'Australia',
                   '3': 'Brazil',
                   '4': 'Colombia',
@@ -19,7 +19,6 @@ countries = dict({'1': 'Argentina',
                   '18': 'Portugal',
                   '19': 'United Arab Emirates',
                   '20': 'United Kingdom'})
-
 
 serialization_option = "XML"
 deserialization_option = "XML"
--- Server.py
+++ Server.py
@@ -1,48 +1,72 @@
 # Server
+import json
 import os
-import socket
+import pickle
+from pickle import dumps, loads, load, dump
 
+import socket
+from dict2xml import dict2xml
+from cryptography.fernet import Fernet
+
+
+#from encryption_helper import EncryptionHelper
+#from configparser import NoSectionError, NoOptionError
 import Config
 
-s = socket.socket()
-print('Socket created')
-s.bind((Config.server_address, Config.port_number))
-s.listen(3)  # take it out or put it into the config file (config.timeout(3))
-print('Waiting for connections')
 
 # Deserialization
 def des(deserialization):
     if Config.deserialization_option == "JSON":
         return json.loads(deserialization)
-
     elif Config.deserialization_option == "BINARY":
-        return pickle.loads(deserialization)
-
+        return loads(deserialization)
     elif Config.deserialization_option == "XML":
         return dict2xml(deserialization, wrap="dictionary", indent="  ")
-
     else:
         print("format not recognised")
-        
+
+def ser(serialization):
+    if Config.serialization_option == "JSON":
+        return json.dumps(serialization)
+    elif Config.serialization_option == "BINARY":
+        return pickle.dumps(serialization)
+    elif Config.serialization_option == "XML":
+        return dict2xml(serialization, wrap="dictionary", indent="  ")
+    else:
+        print("format not recognised")
+
+
 
 def decrypt():
     f = Fernet(Config.key)
     text = input("GrpC.txt").encode()
     encrypted = f.encrypt(text)
     decrypted = f.decrypt(encrypted)
-    
+def encrypt():
+    f = Fernet(Config.key)
+    text = input("GrpC.txt").encode()
+    encrypted = f.encrypt(text)
+    print("Encrypted dictionary")
 
-payload = des(COUNTRIES)
-s.recv(payload)
-while True:
-       c, addr = s.accept()
-    name = c.recv(1024).decode()  # put the number into the config file # print("connected with ", addr, name)
-    if Config.opt == 1:
-        print(name)
-    else:
-        sys.exit()
-        
-    c.send(bytes('Welcome to the dictionary', 'utf-8'))  # messages into the config file
-    c.close()
 
-    
+payload = des(Config.Countries)
+payload_en = ser(Config.Countries)
+
+Cont = payload
+otp = input('Where do you want to print it (1 to console, 2 to a XML, 3 to a TXT encrypted, 4 to a TXT, 5 to a JSON)?: ')
+if otp == '1':
+    print(payload)
+elif otp == '2':
+    with open('GrpC.XML', mode='a') as Config.Countries:
+        print(payload, file=Config.Countries)
+elif otp == '3':
+    with open('GrpC_encrypt.txt', mode='a') as Config.Countries:
+        print(payload_en, file=Config.Countries)
+elif otp == '4':
+    with open('GrpC.txt', mode='a') as Config.Countries:
+        print(payload, file=Config.Countries)
+elif otp == '5':
+    with open('GrpC.json', mode='a') as Config.Countries:
+        print(payload, file=Config.Countries)
+else:
+    print('You write an invalid number, please try again')
